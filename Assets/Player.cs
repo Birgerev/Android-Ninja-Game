e0b91e9330dd6fc3b2b4b14e7b813576868524f3
Birger Evansson <bibbevidio@gmail.com>
2018-07-26 11:19:27 +0200

    basics

---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Manager manager;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void Controls()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            Dash(new Vector2(-1, 0));
+        if (Input.GetKeyDown(KeyCode.D))
+            Dash(new Vector2(-1, 0));
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    public void Dash(Vector2 dir)
+    {
+
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public void Throw(Vector2 dir)
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+}
---
+++ Assets/RigidbodyPixel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyPixel : MonoBehaviour {
+
+    public float pixelSize = 0.0625f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        pos.x = pixelSize * Mathf.RoundToInt(pos.x / pixelSize);
+        pos.y = pixelSize * Mathf.RoundToInt(pos.y / pixelSize);
+        pos.z = 0;
+
+
+        transform.position = pos;
+	}
+}
