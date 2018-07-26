9f2d02eca71af2b538b25b90e6b1f998606cea3b
Birger Evansson <bibbevidio@gmail.com>
2018-07-26 12:44:30 +0200

    PC Controls

---
+++ Assets/TapTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTrigger : MonoBehaviour {
+
+    public Vector2 dir;
+    
+    void OnMouseDown()
+    {
+        GameObject.Find("Ninja").GetComponent<Player>().Throw(dir);
+    }
+}
