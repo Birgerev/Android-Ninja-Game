4558a636276e048d503114615fa5a9941c4404f8
Birger Evansson <bibbevidio@gmail.com>
2018-08-21 20:07:11 +0200

    Revert "Add Belts To Features Branch"

--- Assets/Scripts/BeltManager.cs
+++ Assets/Scripts/BeltManager.cs
@@ -7,14 +7,11 @@ public class BeltManager : MonoBehaviour {
     public static Color color;
 
     public Color[] colors;
-    public int[] highscoreForUnlock;
     public int selectedColor;
-
-    public static BeltManager instance;
 
     // Use this for initialization
     void Start () {
-        instance = this;
+
     }
 	
 	// Update is called once per frame
