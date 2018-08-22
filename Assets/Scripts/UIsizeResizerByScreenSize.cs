8ac758f38cf489229c5f45f6a1d64067e0b009da
Birger Evansson <bibbevidio@gmail.com>
2018-08-22 18:17:09 +0200

    UI adapts to screen size

---
+++ Assets/Scripts/UIsizeResizerByScreenSize.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIsizeResizerByScreenSize : MonoBehaviour {
+
+    public float myX = 1920;
+    public float myY = 1080;
+
+    public float thierX;
+    public float thierY;
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        thierX = Screen.width;
+        thierY = Screen.height;
+
+        print(thierX + " . " + thierY);
+        print(myX + " / " + myY);
+
+        print("this is: " + thierX / myX);
+        float percentageX = thierX / myX;
+        //float sizeX = ((percentageX / 10) * myX) / 100;
+
+
+        float percentageY = thierY / myY;
+        //float sizeY = ((percentageY / 10) * myY) / 100;
+
+        print(percentageX + " : " + percentageY);
+
+        transform.localScale = new Vector2(percentageX, percentageY);
+    }
+}
